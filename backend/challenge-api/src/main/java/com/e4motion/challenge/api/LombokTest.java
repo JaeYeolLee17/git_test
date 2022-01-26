c52fa0ce1ff2b5cf13d5deb2f83ea4f3a2caa7e6
sjkim <sjkim@e4motion.com>
2022-01-26 17:24:50 +0900

    apply lombok lib for api.

---
+++ backend/challenge-api/src/main/java/com/e4motion/challenge/api/LombokTest.java
@@ -0,0 +1,15 @@
+package com.e4motion.challenge.api;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+public class LombokTest {
+	private int a;
+	private String b;
+}
--- backend/challenge-auth/src/main/java/com/e4motion/challenge/auth/ProfileController.java
+++ backend/challenge-auth/src/main/java/com/e4motion/challenge/auth/ProfileController.java
@@ -4,8 +4,6 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.e4motion.common.MyService;
-
 @RestController
 public class ProfileController {
 

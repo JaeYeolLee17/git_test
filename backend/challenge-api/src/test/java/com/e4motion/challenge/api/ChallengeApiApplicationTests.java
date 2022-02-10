af1ad3e431899973e51ffcf644ad17e629b49617
sjkim <sjkim@e4motion.com>
2022-02-10 18:21:20 +0900

    adding mapper test, repository test.

--- backend/challenge-api/src/test/java/com/e4motion/challenge/api/ChallengeApiApplicationTests.java
+++ backend/challenge-api/src/test/java/com/e4motion/challenge/api/ChallengeApiApplicationTests.java
@@ -1,21 +1,13 @@
 package com.e4motion.challenge.api;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-
-import com.e4motion.common.MyService;
 
 @SpringBootTest
 class ChallengeApiApplicationTests {
 
-	@Autowired
-	private MyService myService;
-
 	@Test
 	public void contextLoads() {
-		assertThat(myService.message()).isNotNull();
 	}
+	
 }
---
+++ backend/challenge-api/src/test/java/com/e4motion/challenge/api/domain/mapper/UserMapperTests.java
@@ -0,0 +1,106 @@
+package com.e4motion.challenge.api.domain.mapper;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import org.junit.jupiter.api.Test;
+import org.mapstruct.factory.Mappers;
+
+import com.e4motion.challenge.api.domain.dto.UserDto;
+import com.e4motion.challenge.api.domain.entity.Authority;
+import com.e4motion.challenge.api.domain.entity.User;
+
+public class UserMapperTests {
+
+	private UserMapper mapper = Mappers.getMapper(UserMapper.class);
+	
+	@Test
+    public void mapperTests() {
+		
+		String testUserId = "admin";
+		String testUsername = "adminname";
+		String testEmail = "admin@email...";
+		String testPhone = "01022223333";
+		String testAuthority = Authority.ROLE_ADMIN;
+		
+		Set<Authority> authorities = new HashSet<>();
+		authorities.add(new Authority(testAuthority));
+		User user = User.builder()
+				.userId(testUserId)
+				.username(testUsername)
+				.email(testEmail)
+				.phone(testPhone)
+				.authorities(authorities)
+				.build();
+		
+		UserDto userDto = mapper.toUserDto(user);
+		
+		assertThat(userDto.getUserId()).isEqualTo(testUserId);
+		assertThat(userDto.getUsername()).isEqualTo(testUsername);
+		assertThat(userDto.getEmail()).isEqualTo(testEmail);
+		assertThat(userDto.getPhone()).isEqualTo(testPhone);
+		assertThat(userDto.getAuthority()).isEqualTo(testAuthority);
+		
+	}
+	
+	@Test
+    public void mapperListTests() {
+		
+		String testUserId = "admin";
+		String testUsername = "adminname";
+		String testEmail = "admin@eamil...";
+		String testPhone = "01022223333";
+		String testAuthority = Authority.ROLE_ADMIN;
+		
+		String testUserId2 = "user1";
+		String testUsername2 = "user1name";
+		String testEmail2 = "user1@email...";
+		String testPhone2 = "01044445555";
+		String testAuthority2 = Authority.ROLE_USER;
+		
+		Set<Authority> authorities = new HashSet<>();
+		authorities.add(new Authority(testAuthority));
+		User user = User.builder()
+				.userId(testUserId)
+				.username(testUsername)
+				.email(testEmail)
+				.phone(testPhone)
+				.authorities(authorities)
+				.build();
+		
+		Set<Authority> authorities2 = new HashSet<>();
+		authorities2.add(new Authority(testAuthority2));
+		User user2 = User.builder()
+				.userId(testUserId2)
+				.username(testUsername2)
+				.email(testEmail2)
+				.phone(testPhone2)
+				.authorities(authorities2)
+				.build();
+		
+		List<User> users = new ArrayList<>();
+		users.add(user);
+		users.add(user2);
+		
+		List<UserDto> userDtos = mapper.toUserDto(users);
+		
+		assertThat(userDtos.size()).isEqualTo(2);
+		
+		assertThat(userDtos.get(0).getUserId()).isEqualTo(testUserId);
+		assertThat(userDtos.get(0).getUsername()).isEqualTo(testUsername);
+		assertThat(userDtos.get(0).getEmail()).isEqualTo(testEmail);
+		assertThat(userDtos.get(0).getPhone()).isEqualTo(testPhone);
+		assertThat(userDtos.get(0).getAuthority()).isEqualTo(testAuthority);
+		
+		assertThat(userDtos.get(1).getUserId()).isEqualTo(testUserId2);
+		assertThat(userDtos.get(1).getUsername()).isEqualTo(testUsername2);
+		assertThat(userDtos.get(1).getEmail()).isEqualTo(testEmail2);
+		assertThat(userDtos.get(1).getPhone()).isEqualTo(testPhone2);
+		assertThat(userDtos.get(1).getAuthority()).isEqualTo(testAuthority2);
+		
+	}
+}

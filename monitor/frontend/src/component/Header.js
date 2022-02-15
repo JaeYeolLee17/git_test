730ba4dd8adcf1c2b4853f212df18d2bb1f0240c
ihbae <inho.a.bae@eraeams.com>
2022-02-15 16:08:17 +0900

    [monitor] add kakao map / overlay camera

--- monitor/frontend/src/component/Header.js
+++ monitor/frontend/src/component/Header.js
@@ -1,13 +1,18 @@
 import React from "react";
-import { useLocation } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 const Header = () => {
-    const location = useLocation();
-    console.log(location.pathname);
+    const navigate = useNavigate();
+
+    const handleLogout = () => {
+        localStorage.removeItem("currentUser");
+        navigate("/login");
+    };
 
     return (
         <div style={{ color: "red", border: "solid 1px", background: "blue" }}>
             <h1>Header</h1>
+            <button onClick={handleLogout}>Logout</button>
         </div>
     );
 };

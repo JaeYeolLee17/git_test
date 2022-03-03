f64f41cc439159058e974ba02e49dbe18f647af6
ihbae <inho.a.bae@eraeams.com>
2022-03-03 19:34:49 +0900

    [monitor] add lastweek & lastmonthavg chart

---
+++ monitor/frontend/src/common.js
@@ -0,0 +1,7 @@
+export const trafficColorNormal = "#00e025";
+export const trafficColorSlowly = "#ff8800";
+export const trafficColorBusy = "#df0900";
+export const trafficColorBorder = "#ffffff";
+
+export const trafficSpeedNormal = 30;
+export const trafficSpeedBusy = 25;
--- monitor/frontend/src/string.js
+++ monitor/frontend/src/string.js
@@ -1,3 +1,8 @@
 export const chart_no_data = "조회된 데이터 없음";
+export const chart_lastweek = "지난 주";
+export const chart_lastmonthavg = "지난 4주 평균";
 
 export const total = "전체";
+
+export const stats_qtsrlu = "총 차량 이동시간";
+export const stats_srlu = "총 차량 이동거리";

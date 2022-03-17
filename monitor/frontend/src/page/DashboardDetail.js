f385fa4248544fc66212a908dedcb857be09903d
ihbae <inho.a.bae@eraeams.com>
2022-03-17 16:29:08 +0900

    [monitor] add SelectorRegion / SelectorIntersection in DashboardDetail

--- monitor/frontend/src/page/DashboardDetail.js
+++ monitor/frontend/src/page/DashboardDetail.js
@@ -1,13 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "../component/Header";
 import Menu from "../component/Menu";
+import SelectorIntersection from "../component/SelectorIntersection";
+import SelectorRegion from "../component/SelectorRegion";
 
 const DashboardDetail = () => {
+    const [currentRegionInfo, setCurrentRegionInfo] = useState({});
+
+    const onChangedCurrentRegion = (regionItem) => {
+        //console.log("regionItem", regionItem);
+        setCurrentRegionInfo(regionItem);
+    };
+
+    const onChangedCurrentIntersection = (intersectionItem) => {
+        //console.log("intersectionItem", intersectionItem);
+    };
+
     return (
         <div>
             <Header />
             <Menu />
-            DashboardDetail
+            <SelectorRegion onChangedCurrentRegion={onChangedCurrentRegion} />
+
+            <SelectorIntersection
+                currentRegionInfo={currentRegionInfo}
+                onChangedCurrentIntersection={onChangedCurrentIntersection}
+            />
         </div>
     );
 };

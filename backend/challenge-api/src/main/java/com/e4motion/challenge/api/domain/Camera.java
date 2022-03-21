c2090caaf9d6cc495a2af72c12735ee2aac388ff
Jin-Young Yun <jinyoung.yun@eraeams.com>
2022-03-21 09:49:27 +0900

    add db data

--- backend/challenge-api/src/main/java/com/e4motion/challenge/api/domain/Camera.java
+++ backend/challenge-api/src/main/java/com/e4motion/challenge/api/domain/Camera.java
@@ -5,9 +5,17 @@ import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.sql.Timestamp;
 
 @Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 @Entity
 @Table(name = "nt_camera")
 public class Camera {
@@ -16,17 +24,61 @@ public class Camera {
 	@Column(name = "camera_id", length = 10)
 	private String cameraId;
 
-	@Column(name = "password", length = 128, nullable = false)
+	@Column(name = "password", length = 256, nullable = false)
 	private String password;
 	
 	@Column(name = "intersection_id", length = 10)
 	private String intersectionId;
-	
-	
-	// TODO: more columns...
-	
-   
+
+	@Column(name = "lat")
+	private double lat;
+
+	@Column(name = "lng")
+	private double lng;
+
+	@Column(name = "direction", length = 10)
+	private String direction;
+
+	@Column(name = "rtsp_url", length = 128)
+	private String rtspUrl;
+
+	@Column(name = "server_url", length = 128)
+	private String serverUrl;
+
+	@Column(name = "collect_cycle")
+	private Integer collectCycle;
+
+	@Column(name = "rtsp_id", length = 10)
+	private String rtspId;
+
+	@Column(name = "rtsp_password", length = 256, nullable = false)
+	private String rtspPassword;
+
+	@Column(name = "send_cycle")
+	private Integer sendCycle;
+
+	@Column(name = "distance")
+	private Integer distance;
+
+	@Column(name = "last_data_time")
+	private Timestamp lastDataTime;
+
+	@Column(name = "s_width")
+	private Integer smallWidth;
+
+	@Column(name = "s_height")
+	private Integer smallHeight;
+
+	@Column(name = "l_width")
+	private Integer largeWidth;
+
+	@Column(name = "l_height")
+	private Integer largeHeight;
+
+	@Column(name = "degree")
+	private Integer degree;
+
    	@Column(name = "settings_updated")
    	private boolean settingsUpdated;
-   
+
 }
--- backend/challenge-api/src/main/resources/schema-postgresql.sql
+++ backend/challenge-api/src/main/resources/schema-postgresql.sql
@@ -1,7 +1,11 @@
 DROP TABLE IF EXISTS public.nt_user_authority CASCADE;
 DROP TABLE IF EXISTS public.nt_authority CASCADE;
 DROP TABLE IF EXISTS public.nt_user CASCADE;
+DROP TABLE IF EXISTS public.nt_region CASCADE;
 DROP TABLE IF EXISTS public.nt_camera CASCADE;
+DROP TABLE IF EXISTS public.nt_intersection CASCADE;
+DROP TABLE IF EXISTS public.nt_link CASCADE;
+
 
 CREATE TABLE public.nt_authority
 (
@@ -37,8 +41,56 @@ CREATE TABLE public.nt_user_authority
 CREATE TABLE public.nt_camera
 (
     camera_id character varying(10) COLLATE pg_catalog."default" NOT NULL,
-    intersection_id character varying(10) COLLATE pg_catalog."default",
-    password character varying(128) COLLATE pg_catalog."default" NOT NULL,
-    settings_updated boolean DEFAULT false,
-    CONSTRAINT nt_camera_pkey PRIMARY KEY (camera_id)
+    intersection_id character varying(10) COLLATE pg_catalog."default" NOT NULL,
+    lat double precision NOT NULL,
+    lng double precision NOT NULL,
+    direction character varying(10) COLLATE pg_catalog."default" NOT NULL,
+    rtsp_url character varying(128) COLLATE pg_catalog."default",
+    server_url character varying(128) COLLATE pg_catalog."default",
+    collect_cycle integer,
+    password character varying(256) COLLATE pg_catalog."default",
+    rtsp_id character varying(10) COLLATE pg_catalog."default",
+    rtsp_password character varying(256) COLLATE pg_catalog."default",
+    send_cycle integer,
+    distance integer,
+    settings_updated boolean,
+    last_data_time timestamp without time zone,
+    s_width integer,
+    s_height integer,
+    l_width integer,
+    l_height integer,
+    degree integer,
+    CONSTRAINT nt_camera_pkey PRIMARY KEY (camera_id),
+    CONSTRAINT u_camera_intersection_id UNIQUE (camera_id, intersection_id)
+);
+
+CREATE TABLE public.nt_region
+(
+    region_id character varying(10) COLLATE pg_catalog."default" NOT NULL,
+    region_name character varying(20) COLLATE pg_catalog."default" NOT NULL,
+    gps point[],
+    CONSTRAINT nt_region_pkey PRIMARY KEY (region_id)
+);
+
+CREATE TABLE public.nt_intersection
+(
+    intersection_id character varying(10) COLLATE pg_catalog."default" NOT NULL,
+    intersection_name character varying(20) COLLATE pg_catalog."default" NOT NULL,
+    lat double precision NOT NULL,
+    lng double precision NOT NULL,
+    region_id character varying(10) COLLATE pg_catalog."default" NOT NULL,
+    national_id integer,
+    CONSTRAINT nt_intersection_pkey PRIMARY KEY (intersection_id)
+);
+
+CREATE TABLE public.nt_link
+(
+    start_id character varying(10) COLLATE pg_catalog."default" NOT NULL,
+    end_id character varying(10) COLLATE pg_catalog."default" NOT NULL,
+    start_name character varying(30) COLLATE pg_catalog."default" NOT NULL,
+    end_name character varying(30) COLLATE pg_catalog."default" NOT NULL,
+    gps point[] NOT NULL,
+    link_id character varying(10) COLLATE pg_catalog."default" NOT NULL,
+    CONSTRAINT nt_link_pkey PRIMARY KEY (link_id),
+    CONSTRAINT "u_startId_endId" UNIQUE (start_id, end_id)
 );

7f899a1068adca774aa31909e0edca77c70f7758
Sean Choi <yhchoi@emgglobal.co.kr>
2022-03-28 16:32:47 +0900

    [mantine] Add test files for remote work

--- monitor/frontend/src/component/Menu.js
+++ monitor/frontend/src/component/Menu.js
@@ -5,7 +5,7 @@ import * as Common from "../commons/common";
 
 const Menu = () => {
     return (
-        <div style={{ border: "solid 1px", background: "red" }}>
+        <div >
             <ul>
                 <li>
                     <Link to={Common.PAGE_DASHBOARD}>Dashboard</Link>
@@ -18,6 +18,11 @@ const Menu = () => {
                 <li>
                     <Link to={Common.PAGE_STAT_TRAFFIC}>Stat Traffic</Link>
                 </li>
+                
+                <li>                    
+                    <Link to={"/dashboard/test"}> TEST </Link>
+                </li>
+
             </ul>
         </div>
     );

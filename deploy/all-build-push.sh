a713fcee531ec9a28a943e58bbcb2e55866e1ce3
sjkim <sjkim@e4motion.com>
2022-04-06 22:17:03 +0900

    add tag.

--- deploy/all-build-push.sh
+++ deploy/all-build-push.sh
@@ -1,16 +1,6 @@
 #!/bin/bash
 
-if [ $# -ne 1 ]; then
-  echo "----------------------------------"
-  echo "Usage: $0 VERSION"
-  echo "----------------------------------"
-  echo "Ex:"
-  echo "$ $0 1.0.0"
-  echo "----------------------------------"
-  exit -1
-fi
-
-export TAG=$1
+export TAG=1.0.0
 
 cd deploy
 ./challenge-build.sh

61f8b22a5c3818db39c15fbfec639ea34d9c02c9
sjkim <sjkim@e4motion.com>
2022-07-18 19:24:00 +0900

    data-provider : save as file

--- backend/challenge-data-provider/src/main/java/com/e4motion/challenge/data/provider/controller/DataController.java
+++ backend/challenge-data-provider/src/main/java/com/e4motion/challenge/data/provider/controller/DataController.java
@@ -15,8 +15,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletResponse;
 import javax.validation.constraints.Max;
 import javax.validation.constraints.Min;
+import java.io.IOException;
 import java.time.LocalDateTime;
 import java.util.HashMap;
 
@@ -31,14 +33,43 @@ public class DataController {
 
     private final DataService dataService;
 
-    @Operation(summary = "데이터", description = "접근 권한 : 데이터 사용자")
+    @Operation(summary = "데이터", description = "접근 권한 : 데이터 사용자, 최대 한도 : 10만건")
     @PreAuthorize("hasRole('ROLE_DATA')")
     @GetMapping("/data")
-    public Response query(@RequestParam(value = "startTime", required = true) @DateTimeFormat(pattern = DateTimeHelper.dateTimeFormat) LocalDateTime startTime,
+    public Response get(@RequestParam(value = "startTime", required = true) @DateTimeFormat(pattern = DateTimeHelper.dateTimeFormat) LocalDateTime startTime,
+                        @RequestParam(value = "endTime", required = false) @DateTimeFormat(pattern = DateTimeHelper.dateTimeFormat) LocalDateTime endTime,
+                        @RequestParam(value = "limit", required = true) @Min(1) @Max(MAX_LIMIT) Integer limit,
+                        @RequestParam(value = "filterBy", required = false) String filterBy,
+                        @RequestParam(value = "filterId", required = false) String filterId) {
+
+        DataListDto dataList = dataService.get(getRequestMap(startTime, endTime, limit, filterBy, filterId));
+
+        Response response = new Response("data", dataList.getData());
+        response.setData("nextTime", dataList.getNextTime());
+
+        return response;
+    }
+
+    @Operation(summary = "데이터 파일", description = "접근 권한 : 데이터 사용자, 최대 한도 : 10만건")
+    @PreAuthorize("hasRole('ROLE_DATA')")
+    @GetMapping(value = "/data/file", produces = "text/csv")
+    public void getAsFile(@RequestParam(value = "startTime", required = true) @DateTimeFormat(pattern = DateTimeHelper.dateTimeFormat) LocalDateTime startTime,
                           @RequestParam(value = "endTime", required = false) @DateTimeFormat(pattern = DateTimeHelper.dateTimeFormat) LocalDateTime endTime,
                           @RequestParam(value = "limit", required = true) @Min(1) @Max(MAX_LIMIT) Integer limit,
                           @RequestParam(value = "filterBy", required = false) String filterBy,
-                          @RequestParam(value = "filterId", required = false) String filterId) {
+                          @RequestParam(value = "filterId", required = false) String filterId,
+                          HttpServletResponse response) throws IOException {
+
+        response.setCharacterEncoding("UTF-8");
+        response.setContentType("text/csv; charset=UTF-8");
+
+        String filename = "data-" + LocalDateTime.now() + ".csv";
+        response.setHeader("Content-disposition", "attachment;filename=" + filename);
+
+        dataService.write(getRequestMap(startTime, endTime, limit, filterBy, filterId), response.getWriter());
+    }
+
+    private HashMap<String, Object> getRequestMap(LocalDateTime startTime, LocalDateTime endTime, Integer limit, String filterBy, String filterId) {
 
         HashMap<String, Object> map = new HashMap<>();
         map.put("startTime", startTime.toString());
@@ -50,13 +81,6 @@ public class DataController {
             map.put("filterBy", filterBy);
             map.put("filterId", filterId);
         }
-
-        DataListDto dataList = dataService.query(map);
-
-        Response response = new Response("data", dataList.getData());
-        response.setData("nextTime", dataList.getNextTime());
-
-        return response;
+        return map;
     }
-
 }

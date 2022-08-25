9f9d600ad51f99427c5d998ec702f90604215505
ydjo <ydjo@emgglobal.co.kr>
2022-08-25 15:10:44 +0900

    [challenge-api] change weather controller, yml

--- backend/challenge-api/src/main/java/com/e4motion/challenge/api/controller/WeatherController.java
+++ backend/challenge-api/src/main/java/com/e4motion/challenge/api/controller/WeatherController.java
@@ -3,6 +3,7 @@ package com.e4motion.challenge.api.controller;
 import com.e4motion.challenge.common.response.Response;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,8 +17,14 @@ import java.util.concurrent.ConcurrentHashMap;
 @RestController
 @RequestMapping(path = "v2")
 public class WeatherController {
-    private final String BASE_URL = "http://api.openweathermap.org/data/2.5/weather";
-    private final String API_KEY = "4022a4ec0f8afcdaa8d83c019c98ce8e";
+
+    private final String location = "?q=";
+    private final String appId = "&appid=";
+
+    @Value("${openweather.api-key}")
+    String apiKey;
+    @Value("${openweather.base-url}")
+    String baseUrl;
 
     @Operation(summary = "날씨 API 데이터", description = "접근 권한 : 최고관리자, 운영자, 사용자")
     @PreAuthorize("hasAnyRole('ROLE_ADMIN', 'ROLE_MANAGER', 'ROLE_USER')")
@@ -29,16 +36,16 @@ public class WeatherController {
 
         URI uri = getUri(location);
 
-        return new Response("weather", new RestTemplate().getForObject(uri, ConcurrentHashMap.class));
+        RestTemplate template = new RestTemplate();
+        ConcurrentHashMap weather = template.getForObject(uri, ConcurrentHashMap.class);
+
+        return new Response("weather", weather);
 
     }
 
     private URI getUri(String selectedLocation) throws Exception {
 
-        String location = "?q=";
-        String apiKey = "&appid=";
-
-        String url = BASE_URL + location + Location.valueOf(selectedLocation) + apiKey + API_KEY;
+        String url = baseUrl + location + Location.valueOf(selectedLocation) + appId + apiKey;
 
         return new URI(url);
     }
